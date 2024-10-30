1617974633e62f6964b42f40148f8d84da7fc4e8
Paulo Cesar <149436023+paulocesargit@users.noreply.github.com>
2024-10-29 22:27:30 -0300

    Add files via upload

---
+++ assets/js/projetos.js
@@ -0,0 +1,108 @@
+function projetos(section) {
+  section.innerHTML = `<header>
+      <h2>Projetos</h2>
+    </header>
+    <div class="content">            
+       <div class="card">
+  <p>
+    App Administrador de Links <br />
+    Desenvolvido com React Native e TypeScript!
+  </p>
+
+  <!-- Link para o repositório do GitHub -->
+  <a href="https://github.com/paulocesargit/Administrador-de-Links" target="_blank">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+    >
+      <path fill="none" d="M0 0H24V24H0z" />
+      <path
+        d="M21 3v2c0 9.627-5.373 14-12 14H5.243C5.08 19.912 5 20.907 5 22H3c0-1.363.116-2.6.346-3.732C3.116 16.974 3 15.218 3 13 3 7.477 7.477 3 13 3c2 0 4 1 8 0zm-8 2c-4.418 0-8 3.582-8 8 0 .362.003.711.01 1.046 1.254-1.978 3.091-3.541 5.494-4.914l.992 1.736C8.641 12.5 6.747 14.354 5.776 17H9c6.015 0 9.871-3.973 9.997-11.612-1.372.133-2.647.048-4.22-.188C13.627 5.027 13.401 5 13 5z"
+        fill="rgba(255,255,255,1)"
+      />
+    </svg>
+    <!-- Texto do link -->
+    Ver repositório
+  </a>
+
+  <!-- Imagem de exemplo -->
+  <img
+    src="./assets/projetos/admlinks.png"
+    alt="Imagem do aplicativo"
+    width="100"
+    height="100"
+  />
+</div>
+<div class="card">
+  <p>
+    Gerador de Senhas Seguro <br />
+    Desenvolvido com HTML | CSS e JavaScript!
+  </p>
+    <path fill="none" d="M0 0H24V24H0z" />
+    <path
+      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"
+      fill="rgba(255,255,255,1)"
+    />
+  </svg>
+
+  <!-- Link para acessar o projeto -->
+  <a href="https://paulocesargit.github.io/Gerador-de-senhas/index.html" target="_blank">
+    Acessar o projeto.
+  </a>
+
+  <!-- Imagem ilustrativa -->
+  <img
+    src="./assets/projetos/geradordesenhas.png"
+    alt="Imagem do gerador de senhas"
+    width="100"
+    height="100"
+  />
+</div>
+<div class="card">
+  <p>
+    Portifolio para o instagram <br />
+    Desenvolvido com HTML | CSS e JavaScript!
+  </p>
+    <path fill="none" d="M0 0H24V24H0z" />
+    <path
+      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"
+      fill="rgba(255,255,255,1)"
+    />
+  </svg>
+
+  <!-- Link para acessar o projeto -->
+  <a href="https://paulocesargit.github.io/Portifolio-instagram/" target="_blank">
+    Acessar o projeto.
+  </a>
+
+  <!-- Imagem ilustrativa -->
+  <img
+    src="./assets/projetos/portifolioinsta.png"
+    alt="Imagem do gerador de senhas"
+    width="100"
+    height="100"
+  />
+</div>
+<div class="card">
+          <p>
+            Em breve outros projetos! ! <br />
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path fill="none" d="M0 0H24V24H0z" />
+            <path
+              d="M21 3v2c0 9.627-5.373 14-12 14H5.243C5.08 19.912 5 20.907 5 22H3c0-1.363.116-2.6.346-3.732C3.116 16.974 3 15.218 3 13 3 7.477 7.477 3 13 3c2 0 4 1 8 0zm-8 2c-4.418 0-8 3.582-8 8 0 .362.003.711.01 1.046 1.254-1.978 3.091-3.541 5.494-4.914l.992 1.736C8.641 12.5 6.747 14.354 5.776 17H9c6.015 0 9.871-3.973 9.997-11.612-1.372.133-2.647.048-4.22-.188C13.627 5.027 13.401 5 13 5z"
+              fill="rgba(255,255,255,1)"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>`;
+}
+export { projetos };
